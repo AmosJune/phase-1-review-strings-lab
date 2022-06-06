d12199a14f8d19ed62e4b03c617a7c22c07931c3
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-06-06 16:00:17 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Amos';
+
+const welcomeMessage = "Welcome to Flatbook, Amos!";
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
